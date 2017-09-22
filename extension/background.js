d33d1135f67687da54e7f1ed8eb8a245bbe1c529
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-09-22 09:27:29 -0500

    Log about disconnect errors

--- extension/background.js
+++ extension/background.js
@@ -4,6 +4,10 @@ port.onMessage.addListener((response) => {
   console.log("Received: " + response);
 });
 
+port.onDisconnect.addListener(() => {
+  console.log("Port was disconnected. Error:", chrome.runtime.lastError);
+});
+
 chrome.contextMenus.create({
   title: 'Open in default browser',
   contexts: ['link'],
